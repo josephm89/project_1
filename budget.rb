df9ebd5e52efcfc7b37ea725c4df88f22488c891
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-02-24 14:07:22 +0000

    show new transaction page

--- budget.rb
+++ budget.rb
@@ -11,3 +11,8 @@ get '/transactions' do
   @transactions = Transaction.all
   erb(:"transactions/index")
 end
+
+get '/transactions/new' do
+  @transactions = Transaction.all
+  erb(:"transactions/new")
+end
