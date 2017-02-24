c676bf659bbda4a609395d009666aae93bce42ac
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-02-24 10:39:17 +0000

    add missing value ^.^

--- models/transaction.rb
+++ models/transaction.rb
@@ -4,6 +4,7 @@ class Transaction
     @id = options['id'].to_i if options['id']
     @description = options['description']
     @tag_id = options['tag_id']
+    @value = options['value'].to_i
   end
 
 end
