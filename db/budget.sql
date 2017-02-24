1fcc66f2be2a5d25d1a3141cca179d2b34383cfa
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-02-24 10:23:52 +0000

    add tables

---
+++ db/budget.sql
@@ -0,0 +1,13 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+
+CREATE TABLE tags (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  );
+
+CREATE TABLE transactions (
+  id SERIAL8 PRIMARY KEY,
+  description VARCHAR(255),
+  tag_id INT4 REFERENCES tags(id)
+);
