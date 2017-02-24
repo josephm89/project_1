deceb95a034184f6cf228f73e3532e4b3b1c34fa
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-02-24 11:04:29 +0000

    add save function

--- models/tag.rb
+++ models/tag.rb
@@ -1,8 +1,16 @@
+require_relative'../db/sqlrunner'
 class Tag
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
   end
-  
+
+  def save
+    sql = "INSERT INTO tags (name) VALUES ('#{@name}') RETURNING *"
+    @id = SqlRunner.run(sql).first['id']
+  end
+
+
+
 end
